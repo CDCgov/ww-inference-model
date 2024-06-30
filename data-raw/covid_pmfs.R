9f8ad5d94e56f4975e8625dc402f5a0d911f820c
kaitejohnson <uox1@cdc.gov>
2024-06-30 14:23:30 -0400

    add internal package data for covid GI and delay

---
+++ data-raw/covid_pmfs.R
@@ -0,0 +1,24 @@
+params <- get_params(
+  system.file("extdata", "example_params.toml",
+    package = "wwinference"
+  )
+)
+
+generation_interval <- withr::with_seed(42, {
+  wwinference::simulate_double_censored_pmf(
+    max = params$gt_max, meanlog = params$mu_gi,
+    sdlog = params$sigma_gi, fun_dist = rlnorm, n = 5e6
+  ) |> cfaforecastrenewalww::drop_first_and_renormalize()
+})
+
+inc <- wwinference::make_incubation_period_pmf(
+  params$backward_scale, params$backward_shape, params$r
+)
+sym_to_hosp <- wwinference::make_hospital_onset_delay_pmf(
+  params$neg_binom_mu,
+  params$neg_binom_size
+)
+inf_to_hosp <- wwinference::make_reporting_delay_pmf(inc, sym_to_hosp)
+
+usethis::use_data(generation_interval, overwrite = TRUE)
+usethis::use_data(inf_to_hosp, overwrite = TRUE)
