4cccf18ac01d1db98d972ed0784c982ea90b26c5
kaitejohnson <uox1@cdc.gov>
2024-07-08 08:48:57 -0400

    add ar1 testing functions

---
+++ tests/testthat/setup.R
@@ -0,0 +1,22 @@
+testthat_stan_include <- function() {
+  system.file(
+    "stan",
+    package = "wwinference"
+  )
+}
+
+model_file_path_id <- system.file(
+  "stan", "wwinference.stan",
+  package = "wwinference"
+)
+
+cat("\nsetup.R is compiling the stan model in preparation for testing.\n")
+
+# precompiled site-level infection dynamics model
+compiled_site_inf_model <- cmdstanr::cmdstan_model(
+  model_file_path_id,
+  force_recompile = TRUE,
+  compile_standalone = TRUE,
+  include = testthat_stan_include(),
+  dir = tempdir()
+)
