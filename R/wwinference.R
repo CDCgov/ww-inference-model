0c2da4add322ec19a7bff4d65d8fe66c88ee406d
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2024-08-13 09:25:10 -0400

    Update R/wwinference.R
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- R/wwinference.R
+++ R/wwinference.R
@@ -63,7 +63,7 @@
 wwinference <- function(ww_data,
                         count_data,
                         model_spec = wwinference::get_model_spec(),
-                        mcmc_options = wwinference::get_mcmc_options(),
+                        mcmc_options = get_mcmc_options(),
                         generate_initial_values = TRUE,
                         compiled_model = compile_model()) {
   # Check that data is compatible with specifications
