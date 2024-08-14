01985400d398b3eec05982a0ce182aedb011eafc
kaitejohnson <uox1@cdc.gov>
2024-08-14 08:53:41 -0400

    add example to wwinference wrapper function

--- R/wwinference.R
+++ R/wwinference.R
@@ -40,7 +40,6 @@
 #' like extract posterior draws, get diangostic behavior, and plot results
 #' (for example). If the model runs, this
 #' function will return:
-
 #' `fit`: The CmdStan object that is returned from the call to
 #' `cmdstanr::sample()`. Can be used to access draws, summary, diagnostics, etc.
 #' `input_data`: a list containing the input `ww_data` and the input
@@ -58,6 +57,75 @@
 #' @name wwinference
 #'
 #' @export
+#' @examples
+#' ww_data <- tibble::tibble(
+#'   date = rep(seq(
+#'     from = lubridate::ymd("2023-08-01"),
+#'     to = lubridate::ymd("2023-11-01"),
+#'     by = "weeks"
+#'   ), 2),
+#'   site = c(rep(1, 14), rep(2, 14)),
+#'   lab = c(rep(1, 28)),
+#'   conc = abs(rnorm(28, mean = 500, sd = 50)),
+#'   lod = c(rep(20, 14), rep(15, 14)),
+#'   site_pop = c(rep(2e5, 14), rep(4e5, 14))
+#' )
+#'
+#' ww_data_preprocessed <- preprocess_ww_data(ww_data,
+#'   conc_col_name = "conc",
+#'   lod_col_name = "lod"
+#' )
+#' input_ww_data <- indicate_ww_exclusions(ww_data_preprocessed)
+#'
+#' hosp_data <- tibble::tibble(
+#'   date = seq(
+#'     from = lubridate::ymd("2023-07-01"),
+#'     to = lubridate::ymd("2023-10-30"),
+#'     by = "days"
+#'   ),
+#'   daily_admits = sample(5:70, 122, replace = TRUE),
+#'   state_pop = rep(1e6, 122)
+#' )
+#'
+#' input_count_data <- preprocess_count_data(
+#'   hosp_data,
+#'   "daily_admits",
+#'   "state_pop"
+#' )
+#'
+#' generation_interval <- to_simplex(c(0.01, 0.2, 0.3, 0.2, 0.1, 0.1, 0.01))
+#' inf_to_count_delay <- to_simplex(c(
+#'   rep(0.01, 12), rep(0.2, 4),
+#'   rep(0.01, 10)
+#' ))
+#' infection_feedback_pmf <- generation_interval
+#'
+#' params <- get_params(
+#'   system.file("extdata", "example_params.toml",
+#'     package = "wwinference"
+#'   )
+#' )
+#' forecast_date <- "2023-11-06"
+#' calibration_time <- 90
+#' forecast_horizon <- 28
+#' include_ww <- 1
+#' ww_fit <- wwinference(input_ww_data,
+#'   input_count_data,
+#'   model_spec = get_model_spec(
+#'     forecast_date = forecast_date,
+#'     calibration_time = calibration_time,
+#'     forecast_horizon = forecast_horizon,
+#'     generation_interval = generation_interval,
+#'     inf_to_count_delay = inf_to_coutn_delay,
+#'     infection_feedback_pmf = infection_feedback_pmf,
+#'     params = params
+#'   ),
+#'   mcmc_options = get_mcmc_options(
+#'     iter_warmup = 250,
+#'     iter_sampling = 250,
+#'     n_chains = 2
+#'   )
+#' )
 #' @rdname wwinference
 wwinference <- function(ww_data,
                         count_data,
