696738fc642130ec03aa1792c2f07ca032485904
cbernalz <cbernalz@uci.edu>
2024-08-19 11:33:04 -0700

    2024-08-19 update : fixing check-package.

--- tests/testthat/test_ww_model.R
+++ tests/testthat/test_ww_model.R
@@ -50,11 +50,9 @@ test_that("Test the wastewater inference model on simulated data.", {
     "sigma_ww_site_sd",
     "p_hosp_w_sd", "t_peak", "dur_shed", "ww_site_mod_sd", "rt", "rt_site_t",
     "p_hosp", "w", "hosp_wday_effect", "eta_i0", "eta_growth",
-    "infection_feedback", "p_hosp_mean", "log_phi_mu", "log_phi_sd",
-    "l", "log_sigma_generalized_mu", "log_sigma_generalized_sd",
-    "log_scaling_factor_mu", "log_scaling_factor_sd"
+    "infection_feedback", "p_hosp_mean", "log_sigma_generalized",
+    "log_phi", "log_scaling_factor"
   )
-
   for (param in model_params) {
     # Compare everything, with numerical tolerance
     testthat::expect_equal(
