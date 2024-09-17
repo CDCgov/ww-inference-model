8404a6ab59d5139890ed13f2da1343cee64a3a65
Dylan H. Morris <dzl1@cdc.gov>
2024-09-17 13:59:58 -0400

    Better fix for test_get_stan_data

--- tests/testthat/test_get_stan_data.R
+++ tests/testthat/test_get_stan_data.R
@@ -91,33 +91,22 @@ test_that(paste0(
   "standard case where sum(site_pops) > total_pop"
 ), {
   input_count_data_mod <- input_count_data
-  stan_data_mod <- expect_no_warning(get_stan_data(
-    input_count_data_mod,
-    input_ww_data,
-    forecast_date,
-    forecast_horizon,
-    calibration_time,
-    generation_interval,
-    inf_to_count_delay,
-    infection_feedback_pmf,
-    params,
-    include_ww
-  ))
-
   input_count_data_mod$total_pop <- sum(unique(input_ww_data$site_pop) - 100)
 
-  expect_warning(get_stan_data(
-    input_count_data_mod,
-    input_ww_data,
-    forecast_date,
-    forecast_horizon,
-    calibration_time,
-    generation_interval,
-    inf_to_count_delay,
-    infection_feedback_pmf,
-    params,
-    include_ww
-  ))
+  expect_warning({
+    stan_data_mod <- get_stan_data(
+      input_count_data_mod,
+      input_ww_data,
+      forecast_date,
+      forecast_horizon,
+      calibration_time,
+      generation_interval,
+      inf_to_count_delay,
+      infection_feedback_pmf,
+      params,
+      include_ww
+    )
+  })
 
   expect_equal(stan_data_mod$n_subpop, (stan_data_mod$n_ww_sites))
   expect_equal(length(stan_data_mod$subpop_size), stan_data_mod$n_subpops)
